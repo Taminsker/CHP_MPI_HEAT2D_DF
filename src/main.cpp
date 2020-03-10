44df2c53518e30c51c44183e599da13ddc03d2ea
Alexis Tardieu <alexis.aikido@hotmail.fr>
2020-03-10 17:30:46 +0100

    Surcharge fonctions sur vecteurs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include "outputinput.h"
+#include "solver.h"
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +11,33 @@ int main(int argc, char *argv[])
     Param P = readParameters ("param.dat");
 
     printf("Nx = %d, Ny = %d, Lx = %f, Ly = %f, D = %f, dt = %f\n", P.Nx, P.Ny, P.Lx, P.Ly, P.D, P.dt);
+
+    std::vector<double> u (5, 1);
+    std::vector<double> v (5, 3);
+    std::vector<double> w (5, 8);
+    std::vector<double> z (4, 2);
+
+    printf("(u|v) = %f\n", (u|v));
+
+    std::vector<double> a = u + v;
+
+    printf("u + v = \n");
+    for (int i = 0; i < a.size(); i++)
+    {
+      printf("%f\n", a [i]);
+    }
+
+    std::vector<double> b = u - v;
+
+    printf("u - v = \n");
+    for (int i = 0; i < b.size(); i++)
+    {
+      printf("%f\n", b [i]);
+    }
+
+    std::vector<double> c = 2.5*v;
+
+    print(c);
 
     return 0;
 }
--- src/solver.h
+++ src/solver.h
@@ -1,4 +1,23 @@
 #ifndef SOLVER_HEAT_2D
 #define SOLVER_HEAT_2D
 
+#include <vector>
+#include <stdio.h>
+
+double operator| (const std::vector<double> &x, const std::vector<double> &y);
+
+std::vector<double> operator+ (const std::vector<double> &x, const std::vector<double> &y);
+
+std::vector<double> operator- (const std::vector<double> &x, const std::vector<double> &y);
+
+void operator+= (std::vector<double> &x, const std::vector<double> &y);
+
+void operator-= (std::vector<double> &x, const std::vector<double> &y);
+
+std::vector<double> operator* (double k, std::vector<double> x);
+
+std::vector<double> operator* (std::vector<double> x, double k);
+
+void print (std::vector<double> a);
+
 #endif// SOLVER_HEAT_2D
