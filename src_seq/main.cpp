4dfc869f18f4f3f0cd27905e8f2dcade53fbb60b
Alexis Tardieu <alexis.aikido@hotmail.fr>
2020-03-22 22:30:49 +0100

    Remplissage main cas test 1

--- src_seq/main.cpp
+++ src_seq/main.cpp
@@ -5,6 +5,7 @@
 #include "operators.h"
 #include "matrix.h"
 #include "listfunctions.h"
+#include "secondmember.h"
 
 #include <cstdlib>
 
@@ -16,85 +17,17 @@ int main(int argc, char *argv[])
     Matrix A;
     A.ReadParamFile ("param.dat");
 
+    // CAS TEST 1
 
-    // TESTS
-    // int N = A.Nx * A.Ny;
-    //
-    // Vector b (N, 0.);
-    //
-    // for (int i = 0; i < A.Nx; ++i)
-    // {
-    //     for (int j = 0; j < A.Ny; ++j)
-    //     {
-    //         b [j * A.Nx + i] = f_1 (i * A.dx, j * A.dy);
-    //     }
-    // }
-    //
-    //
-    // // Vector a (N, 0.5);
-    // // printFile ("test.dat", A * a, A);
-    // // print ("b", b, 9);
-    //
-    // // print ("A", A, 0, 10, 0, 10);
-    //
-    //
-    // Vector x;
-    // x = conj_gradient (A, b);
-    //
-    // // print ("x", x, 9);
-    //
-    //
-    // printFile ("test.dat", x, A);
-    //
-    //
-    // return 0;
+    A.SetStationnaire ();
 
-    // TESTS
-    int N = 10000;
+    Vector b = Set_vector_f (f_1, 0., A);
+    b += Set_vector_b0 (g_1, A);
+    b += Set_vector_b1 (h_1, A);
 
-    Vector u (N, 1);
-    Vector v (N, 3);
-    Vector w (N, 8);
-    Vector z (N, 2);
+    Vector x = conj_gradient (A, b);
 
-    print ("u", u);
-    print ("v", v);
-    print ("w", w);
-    print ("z", z);
-
-    printf ("u|v = %f\n", u|v);
-    print ("u+v", u+v);
-    print ("u-v", u-v);
-    print ("2.5 * u", 2.5 * u);
-    //
-    // // Matrix A;
-    // // A.ReadParamFile ();
-    // // print ("A", A, 0, 10, 250, 260); // affiche A (0:10, 250,260)
-    //
-    // // print ("A", A); // affiche A (0:10, 250,260)
-    //
-    // /// Exemple écriture de fichiers
-    // Vector vec (A.Nx * A.Ny, 1.);
-    // print ("vec", vec, 9);
-    //
-    // vec = A * vec;
-    //
-    // print ("vec", vec, 9);
-
-    // for (int i = 0; i < A.Nx; ++i)
-    // {
-    //     for (int j = 0; j < A.Ny; ++j)
-    //     {
-    //         double x = i * A.dx;
-    //         double y = j * A.dy;
-    //
-    //         int index = j * Nx + i;
-    //
-    //         vec [index] = x*x + y*y;
-    //     }
-    // }
-
-    // printFile ("test.dat", vec, A);
+    printFile ("CasTest1.dat", x, A);
 
     return 0;
 }
