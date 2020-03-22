b6a8a5d93cc3bda8d008bf30fca8300c953b002c
Alexis Tardieu <alexis.aikido@hotmail.fr>
2020-03-22 21:44:42 +0100

    Vector f avec crochets

--- src_seq/secondmember.cpp
+++ src_seq/secondmember.cpp
@@ -7,30 +7,30 @@ std::vector <double> Set_vector_b0 (double (*g) (double, double, double),
 
   for (int i = 0; i < A.Nx; i++)
   {
-    vec_b0 [i] = g ((i + 1) * A.dx, 0.); // Bord inférieur Gamma_0
+    vec_b0 [i] = g ((i + 1) * A.dx, 0., 0.); // Bord inférieur Gamma_0
 
-    vec_b0 [(A.Ny - 1) * A.Nx + i] = g ((i + 1) * A.dx, A.Ly); // Bord supérieur Gamma_0
+    vec_b0 [(A.Ny - 1) * A.Nx + i] = g ((i + 1) * A.dx, A.Ly, 0.); // Bord supérieur Gamma_0
   }
 
-  vec_b0 *= - A.coef_c;
+  vec_b0 *= - A.coeff_c;
 
   return vec_b0;
 }
 
 
 std::vector <double> Set_vector_b1 (double (*h) (double, double, double),
-                                    const Matrix &A);
+                                    const Matrix &A)
 {
   std::vector <double> vec_b1 (A.Nx * A.Ny, 0.);
 
   for (int j = 0; j < A.Ny; j++)
   {
-      vec_b1 [j * A.Nx] = h (0., (j + 1) * A.dy); // Bord gauche Gamma_1
+      vec_b1 [j * A.Nx] = h (0., (j + 1) * A.dy, 0.); // Bord gauche Gamma_1
 
-      vec_b1 [(j + 1) * A.Nx - 1] = h (A.Lx, (j + 1) * A.dy); // Bord droit Gamma_1
+      vec_b1 [(j + 1) * A.Nx - 1] = h (A.Lx, (j + 1) * A.dy, 0.); // Bord droit Gamma_1
   }
 
-  vec_b1 *= - A.coef_b;
+  vec_b1 *= - A.coeff_b;
 
   return vec_b1;
 }
@@ -44,7 +44,7 @@ std::vector <double> Set_vector_f (double (*f) (double, double, double),
   for (int i = 0; i < A.Nx; i++) {
     for (int j = 0; j < A.Ny; j++) {
 
-      vec_f (j * A.Nx + i) = f ( (i + 1) * A.dx, (j + 1) * A.dy, t);
+      vec_f [j * A.Nx + i] = f ( (i + 1) * A.dx, (j + 1) * A.dy, t);
 
     }
   }
