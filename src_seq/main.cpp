4deef60a81d4868160e9871f5615c9c32922a8fb
Alexis Tardieu <alexis.aikido@hotmail.fr>
2020-03-23 14:44:37 +0100

    Switch sur le cas test voulu

--- src_seq/main.cpp
+++ src_seq/main.cpp
@@ -17,21 +17,56 @@ int main(int argc, char *argv[])
     Matrix A;
     A.ReadParamFile ("param.dat");
 
-    // CAS TEST 1
+    int casTest = 0;
+    printf ("Cas test 1, 2 ou 3 : \n");
+    scanf ("%d\n", &casTest);
 
-    A.SetStationnaire ();
+    switch (casTest)
+    {
+      case 1:
+        printf ("Cas stationnaire polynomial\n");
 
-    Vector b = Set_vector_f (f_1, 0., A);
-    // print ("f", Set_vector_f (f_1, 0., A));
-    // print ("g", Set_vector_b0 (g_1, A));
-    // print ("h", Set_vector_b1 (h_1, A));
+        A.SetStationnaire ();
+        Vector b = Set_vector_f (f_1, 0., A);
+        b += Set_vector_b0 (g_1, A);
+        b += Set_vector_b1 (h_1, A);
 
-    b += Set_vector_b0 (g_1, A);
-    b += Set_vector_b1 (h_1, A);
+        Vector x = conj_gradient (A, b);
+        printFile ("sol.dat", x, A);
 
-    Vector x = conj_gradient (A, b);
+        printf ("Pour afficher : tapez 'gnuplot' puis\n'splot using 1:2:3 w p palette'");
 
-    printFile ("CasTest1.dat", x, A);
+        break;
+
+      case 2:
+        printf ("Cas stationnaire sinusoidal\n");
+
+        A.SetStationnaire ();
+        Vector b = Set_vector_f (f_2, 0., A);
+        b += Set_vector_b0 (g_2, A);
+        b += Set_vector_b1 (h_2, A);
+
+        Vector x = conj_gradient (A, b);
+        printFile ("sol.dat", x, A);
+
+        printf ("Pour afficher : tapez 'gnuplot' puis\n'splot using 1:2:3 w p palette'");
+
+        break;
+
+      case 3:
+        printf ("Cas non stationnaire gaussien\n");
+
+        Vector b = Set_vector_f (f_3, 0., A);
+        b += Set_vector_b0 (g_3, A);
+        b += Set_vector_b1 (h_3, A);
+
+        Vector x = conj_gradient (A, b);
+        printFile ("sol.dat", x, A);
+
+        printf ("Pour afficher : tapez 'gnuplot' puis\n'splot using 1:2:3 w p palette'");
+        
+        break;
+    }
 
     return 0;
 }
